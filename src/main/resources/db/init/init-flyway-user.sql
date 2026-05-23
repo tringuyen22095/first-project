bcc1be93ca08853e7f9e5e30b765bfa01a864678
tringuyen22095 <nguyenthtri22095@gmail.com>
2026-05-23 21:48:25 +0700

    Correct init flyway setup

--- src/main/resources/db/init/init-flyway-user.sql
+++ src/main/resources/db/init/init-flyway-user.sql
@@ -1,14 +1,14 @@
 -- Create flyway user for DDL only
 CREATE USER flyway WITH PASSWORD 'flyway_admin';
 GRANT CONNECT ON DATABASE monolithic TO flyway;
-GRANT USAGE, CREATE ON SCHEMA public TO flyway;
 
 -- Create nene user for DML only
 CREATE USER nene WITH PASSWORD '475963218';
 GRANT CONNECT ON DATABASE monolithic TO nene;
-GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA public TO nene;
 
 -- Automatically grant DML to nene user on any table/sequence flyway creates in future.
+GRANT USAGE, CREATE ON SCHEMA public TO flyway;
+
 ALTER DEFAULT PRIVILEGES FOR USER flyway IN SCHEMA public
     GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO nene;
 
